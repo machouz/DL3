1c53d7059e3ceb946c7fb58bd45d71116de40a9f
machou <uzanmacho@gmail.com>
2018-12-10 15:33:36 +0200

    add unic

--- Part2/bad.py
+++ Part2/bad.py
@@ -9,6 +9,7 @@ from experiment import *
 vocab = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd']
 USE_PALYNDROM = True if len(sys.argv) > 1 else False
 
+
 def gen_palyndrom(k=4):
     pos_examples = []
     neg_examples = []
@@ -26,13 +27,17 @@ def gen_palyndrom(k=4):
     return pos_examples, neg_examples
 
 
+# GOOD : any permutation of a^nb^nc^nd^n
+# BAD : the same permutation but with different n for each one
 def gen_series(b=4, k=8):
     pos_examples = []
     neg_examples = []
     for i in range(500):
         rep = random.randint(2, k)
+
         base = list(np.random.permutation(["a", "b", "c", "d"]))
         good = "".join(map(lambda x: x * rep, base))
+
         bad = []
         r = random.randint(2, k)
         for i in base:
@@ -42,6 +47,7 @@ def gen_series(b=4, k=8):
             bad.append(i * nr)
             r = nr
         bad = "".join(bad)
+
         pos_examples.append(good)
         neg_examples.append(bad)
     np.savetxt("pos_a_n_b_n", pos_examples, fmt="%s")
@@ -50,11 +56,36 @@ def gen_series(b=4, k=8):
     return pos_examples, neg_examples
 
 
+# GOOD : any permutation of a^nb^nc^nd^n
+# BAD : any permutation of a^nb^nc^nd^n with a repetition
+def gen_unic(b=4, k=8):
+    pos_examples = []
+    neg_examples = []
+    for i in range(500):
+        rep = random.randint(2, k)
+
+        good_base = list(np.random.permutation(["a", "b", "c", "d"]))
+        good = "".join(map(lambda x: x * rep, good_base))
+
+        bad_base = list(choices("abcd", 4))
+        while len(bad_base) == len(set(bad_base)):
+            bad_base = list(choices("abcd", 4))
+        bad = "".join(map(lambda x: x * rep, bad_base))
+
+        pos_examples.append(good)
+        neg_examples.append(bad)
+    np.savetxt("pos_uniq", pos_examples, fmt="%s")
+    np.savetxt("neg_uniq", neg_examples, fmt="%s")
+
+    return pos_examples, neg_examples
+
+
 if __name__ == '__main__':
     EPOCHS = 100
     word_id = {word: i for i, word in enumerate(vocab)}
 
-    pos_examples, neg_examples = gen_palyndrom() if USE_PALYNDROM else gen_series()
+    pos_examples, neg_examples = gen_series()
+    # pos_examples, neg_examples = gen_palyndrom() if USE_PALYNDROM else gen_series()
 
     pos_examples = map(lambda sentence: sentence2ids(sentence, word_id), pos_examples)
     pos_examples = map(lambda x: torch.LongTensor(x + [1]), pos_examples)
