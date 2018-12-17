2db1c13d0597cfa381e3a098747f920a0343e040
machou <uzanmacho@gmail.com>
2018-12-17 14:59:59 +0200

    transducer1

--- Part3/bilstmTrain.py
+++ Part3/bilstmTrain.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     train_name = sys.argv[1] if len(sys.argv) > 1 else "data/ner/train"
     repr = sys.argv[2] if len(sys.argv) > 2 else "-b"
     model_file = sys.argv[3] if len(sys.argv) > 3 else 'Transducer1_ner'
-    w2i_file = sys.argv[4] if len(sys.argv) > 4 else 'w2i_ner'
-    id_label_file = sys.argv[5] if len(sys.argv) > 5 else 'id_label_ner'
+    w2i_file = sys.argv[4] if len(sys.argv) > 4 else 'Transducer1_ner_i2l'
+    id_label_file = sys.argv[5] if len(sys.argv) > 5 else 'Transducer1_ner_w2i'
     dev_name = sys.argv[6] if len(sys.argv) > 6 else "data/ner/dev"
 
     if repr == "-a":
