098d6c777e95e30f3237830a82bde79017e9f827
machou <uzanmacho@gmail.com>
2018-12-19 18:55:30 +0200

    fix

--- Part3/bilstmTrain.py
+++ Part3/bilstmTrain.py
@@ -6,13 +6,13 @@ import sys
 
 if __name__ == '__main__':
 
-    train_name = sys.argv[1] if len(sys.argv) > 1 else "data/ner/train"
-    repr = sys.argv[2] if len(sys.argv) > 2 else "-b"
-    model_file = sys.argv[3] if len(sys.argv) > 3 else 'Transducer2_ner'
-    w2i_file = sys.argv[4] if len(sys.argv) > 4 else 'Transducer2_ner_w2i'
-    id_label_file = sys.argv[5] if len(sys.argv) > 5 else 'Transducer2_ner_i2l'
+    repr = sys.argv[1] if len(sys.argv) > 1 else "-d"
+    train_name = sys.argv[2] if len(sys.argv) > 2 else "data/ner/train"
+    model_file = sys.argv[3] if len(sys.argv) > 3 else 'Transducer4_ner'
+    w2i_file = sys.argv[4] if len(sys.argv) > 4 else 'Transducer4_ner_w2i'
+    id_label_file = sys.argv[5] if len(sys.argv) > 5 else 'Transducer4_ner_i2l'
     dev_name = sys.argv[6] if len(sys.argv) > 6 else "data/ner/dev"
-    wc2i_file = sys.argv[7] if len(sys.argv) > 7 else "Transducer2_ner_wc2i"
+    wc2i_file = sys.argv[7] if len(sys.argv) > 7 else "Transducer4_ner_wc2i"
 
     if repr == "-a":
         loss_history, accuracy_history = transducer1.train_save(train_name, dev_name, model_file, w2i_file, id_label_file)
